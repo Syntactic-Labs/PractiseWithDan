e26f3927a47dcd95e5d1025597ec5797d117cb85
Michael W <walkerrepair255@yahoo.com>
2022-01-07 15:50:42 -0500

    more prac

--- PractiseWithDan/Program.cs
+++ PractiseWithDan/Program.cs
@@ -6,46 +6,13 @@ namespace PractiseWithDan
     {
         static void Main(string[] args)
         {
-            /*
-            var pyd="#";
-            for (int i = 0; i < 10; i++)
+            Console.WriteLine("insert the number you want the times table for:");
+            int[] times = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            var input = Convert.ToInt32(Console.ReadLine());
+            for (int i = 113; i < 12; i++)
             {
-                Console.WriteLine(pyd);
-                pyd += "#";
-            }*/
-
-            /*for (int i = 1; i < 100; i++)
-            {
-                var result = "";
-                if (i % 3 == 0){ result += "Fizz"; }
-                if (i % 5 == 0){ result += "Buzz"; }
-                if (result == ""){ result += i; }
-                Console.WriteLine(result);
+                Console.WriteLine($"{i}*{input}={ input+ times[i]}");
             }
-            */
-            //var size = 10;
-
-            //for (int i = 0; i < size; i++)
-            //{
-            //    var result = "";
-            //    for (int L = 0; L < size; L++)
-            //    {
-            //        if ((i+L)%2==0){ result += "O"; }
-            //        if ((i+L) % 2==1){ result += "X"; }
-
-            //    }
-            //    Console.WriteLine(result);
-            //}
-
-            int[] Fred = { 1, 2, 3, 4, 5, 56 };
-
-
-            for (int i = 0; i < Fred.Length; i++)
-            {
-                Console.WriteLine($"{Fred[i]},");
-            }
-            
-
         }
     }
 }
