fef5f5641a7b5daa7300039b8f6b7bf2d7326da8
Michael W <walkerrepair255@yahoo.com>
2022-01-08 16:16:55 -0500

    final touches on console times tables

--- PractiseWithDan/Program.cs
+++ PractiseWithDan/Program.cs
@@ -7,29 +7,29 @@ namespace PractiseWithDan
     class Program
     {
         static void Main(string[] args)
-        {
-            C.WriteLine("Insert the number you want the times table for:");
+        {    C.WriteLine("Insert the number you want the times table for:");
             MultiTable();
         }
         private static void MultiTable()
-        {
-            int[] times = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        {   int[] times = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
             var input = C.ReadLine();
-            if (Int32.TryParse(input, out int number)) {    
+            if (Int32.TryParse(input, out int number )) {
+                C.Clear();
+                C.WriteLine($"You choose: {input}");
                 for (int i = 0; i < times.Length; i++) 
                 { C.WriteLine($"{i}*{number}={ number * times[i]}"); }
                 DoOver();
             } else {
+                C.Clear();
                 C.WriteLine("!!!Pick a real number!!!");
                 MultiTable();
             }
         }
         private static void DoOver()
-        {
-            C.WriteLine("Would you like another times table? y/n:");
+        {   C.WriteLine("Would you like another times table? y/n:");
             string answer = C.ReadLine();
-            if (answer == "y") { C.WriteLine("Pick another number!"); MultiTable(); }
-            if (answer != "y" && answer != "n"){C.WriteLine("Read the directions"); DoOver();}
+            if (answer == "y") { C.Clear(); C.WriteLine("Insert the number you want the times table for:"); MultiTable(); }
+            if (answer != "y" && answer != "n") { C.Clear(); C.WriteLine("Read the directions"); DoOver();}
         }
     }
 }
