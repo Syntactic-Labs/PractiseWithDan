acea430bf11b1a15f78145a6c258f71bf27ec661
Michael W <walkerrepair255@yahoo.com>
2022-01-07 21:31:10 -0500

    prac Remove string method(where,howmany)

--- PractiseWithDan/Program.cs
+++ PractiseWithDan/Program.cs
@@ -7,24 +7,10 @@ namespace PractiseWithDan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi cutie! What's your name?!?");
-            var name = Console.ReadLine();
-            Console.WriteLine($"Well hello {name}! A pretty name for a pretty girl.");
-            Thread.Sleep(3000);
-            Console.WriteLine("And how old are you?...");
-            var age = Convert.ToInt32(Console.ReadLine());
-            drinkAge(age);
+            Console.WriteLine(stringRemove("Michael",3));
         }
-        private static void drinkAge(int a)
-        {
-            if (a < 18) { Console.WriteLine("If you're going to lie aleast do it well!!!");
-                Console.WriteLine("try again");
-                drinkAge(Convert.ToInt32(Console.ReadLine())); }
-            if (a > 18 && a < 30){ Console.WriteLine("I did not ask how old you want to be!");
-                Console.WriteLine("try again");
-                drinkAge(Convert.ToInt32(Console.ReadLine()));
-            }
-            if (a==31){ Console.WriteLine("Honesty! Well you look ageless. A real beauty!");}
+        private static string stringRemove(string str, int num) {
+            return str.Remove(num, 3);
         }
     }
 }
