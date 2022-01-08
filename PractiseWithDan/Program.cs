cb35f0c1d791c6d5c8c3c569e87266f4ba5da703
Michael W <walkerrepair255@yahoo.com>
2022-01-08 11:41:04 -0500

    creating times table loop

--- PractiseWithDan/Program.cs
+++ PractiseWithDan/Program.cs
@@ -7,19 +7,29 @@ namespace PractiseWithDan
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            string comma = "";
 
-            for (int i = 0; i < numbers.Length; i++)
+            multiTable();
+
+        }
+        private static void multiTable()
+        {
+            Console.WriteLine("insert the number you want the times table for:");
+            int[] times = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            var input = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < times.Length; i++)
             {
-                Console.Write($"{comma}{numbers[i]}");
-                comma = ", ";
-
-
+                Console.WriteLine($"{i}*{input}={ input * times[i]}");
             }
-            
-            
+            doOver();
             
         }
+            private static void doOver()
+            {
+                Console.WriteLine("Would you like another times table? y/n:");
+                string answer = Convert.ToString(Console.Read());
+
+                if (answer == "y") { multiTable(); }
+                if (answer != "y" || answer != "n"){Console.WriteLine("Read the directions"); doOver();}
+            }
     }
 }
