880586159536165726668b37043b14f4f773a0b3
Michael W <walkerrepair255@yahoo.com>
2022-01-14 19:52:37 -0500

    mutiplay indexed numbers of two arrays

--- PractiseWithDan/Program.cs
+++ PractiseWithDan/Program.cs
@@ -11,10 +11,20 @@ namespace PractiseWithDan
         static void Main(string[] args)
         {   //C.WriteLine("Insert the number you want the times table for:");
             //MultiTable();
-            string hexval = "4C0";
-            Console.WriteLine("Hexadecimal number: " + hexval);
-            int decValue = int.Parse(hexval, System.Globalization.NumberStyles.HexNumber);
-            Console.WriteLine("Decimal number: " + decValue);
+            int[] first_array = { 1, 3, -5, 4 };
+            int[] second_array = { 1, 4, -5, -2 };
+
+            Console.WriteLine("\nArray1: [{0}]", string.Join(", ", first_array));
+            Console.WriteLine("Array2: [{0}]", string.Join(", ", second_array));
+
+            Console.WriteLine("\nMultiply corresponding elements of two arrays: ");
+
+            for (int i = 0; i < first_array.Length; i++)
+            {
+
+                Console.Write(first_array[i] * second_array[i] + " ");
+            }
+            Console.WriteLine("\n");
 
         }
         //private static void MultiTable()
