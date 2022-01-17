fdc984ceb7d94e27a5af734e9de6f79a8a1fe541
Michael W <walkerrepair255@yahoo.com>
2022-01-17 13:03:58 -0500

    Use of ref keyword to change vars value instead of a copy of the value

--- PractiseWithDan/Program.cs
+++ PractiseWithDan/Program.cs
@@ -11,21 +11,20 @@ namespace PractiseWithDan
         static void Main(string[] args)
         {   //C.WriteLine("Insert the number you want the times table for:");
             //MultiTable();
-            int[] first_array = { 1, 3, -5, 4 };
-            int[] second_array = { 1, 4, -5, -2 };
+            var name = "";
+            AskName(ref name);
+            PrintName(name);
 
-            Console.WriteLine($"\nArray1: [{string.Join(", ", first_array)}]");
-            Console.WriteLine("Array2: [{0}]", string.Join(", ", second_array));
-
-            Console.WriteLine("\nMultiply corresponding elements of two arrays: ");
-
-            for (int i = 0; i < first_array.Length; i++)
-            {
-
-                Console.Write(first_array[i] * second_array[i] + " ");
-            }
-            Console.WriteLine("\n");
-
+        }
+        private static void AskName(ref string n)
+        {
+            C.Write("Please sir. What is your name?!?!: ");
+            
+            n = Convert.ToString(C.ReadLine());
+        }
+        private static void PrintName(string n)
+        {
+            C.WriteLine($"Hello {n}");
         }
         //private static void MultiTable()
         //{   int[] times = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
