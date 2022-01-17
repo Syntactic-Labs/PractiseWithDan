922db2968264ef95c89349cc23736a76d0469934
Michael W <walkerrepair255@yahoo.com>
2022-01-17 16:43:21 -0500

    Class creation and call

--- PractiseWithDan/Program.cs
+++ PractiseWithDan/Program.cs
@@ -9,46 +9,12 @@ namespace PractiseWithDan
     class Program
     {
         static void Main(string[] args)
-        {   //C.WriteLine("Insert the number you want the times table for:");
-            //MultiTable();
-            var name = AskName();
-            PrintName(name);
-            name = AskName();
-            PrintName(name);
-        }
-        private static string AskName()
         {
-            C.WriteLine("What is your name: ");
-            return Convert.ToString(C.ReadLine());
-        }
-        private static void PrintName(string n)
-        {
-            C.WriteLine($"hello! {n}");
+            Books book = new("Lord of the Rings", "Jk Rowling", 25, 350, true);
+            Books book1 = new("Fly", "Someguy", 42, 826, true);
+            book1.Print();
+            book.Print();
         }
         
-        //private static void MultiTable()
-        //{   int[] times = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-        //    var input = C.ReadLine();
-        //    if (Int32.TryParse(input, out var number )) {
-        //        C.Clear();
-        //        C.WriteLine($"You choose: {input}");
-        //        for (int i = 0; i < times.Length; i++) 
-        //        { C.WriteLine($"{i}*{number}={ number * times[i]}"); }
-        //        DoOver();
-        //    } else {
-        //        C.Clear();
-        //        C.WriteLine("!!!Pick a real number!!!");
-        //        MultiTable();
-        //    }
-        //}
-        //private static void DoOver()
-        //{   C.WriteLine("Would you like another times table? y/n:");
-        //    string answer = C.ReadLine();
-        //    if (answer == "y") { C.Clear(); C.WriteLine("Insert the number you want the times table for:"); MultiTable(); }
-        //    if (answer != "y" && answer != "n") { C.Clear(); C.WriteLine("Read the directions"); DoOver();}
-        //}
-
-
-
     }
 }
---
+++ PractiseWithDan/Sample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseWithDan
+{
+    class Books
+    {
+        private static int NextId = 1;
+        public int Id { get; set; } = 1;
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Inventory { get; set; } = 0;
+        public int Pages { get; set; }
+        public bool InStock { get; set; }
+        
+        public Books() { }
+        public Books(string title, string author, int inventory, int pages, bool inStock)
+        {  
+            this.Id = NextId++;
+            this.Title = title;
+            this.Author = author;
+            this.Inventory = inventory;
+            this.Pages = pages;
+            this.InStock = inStock;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Id:{this.Id} | Title:{this.Title} | Author:{this.Author} | Inventory:{this.Inventory} | Pages:{this.Pages} | InStock:{this.InStock}");
+            Console.WriteLine("");
+        }
+    }
+}
